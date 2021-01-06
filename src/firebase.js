ce88057e8a9dc5184283f08e456ef96f64525ff7
beerwithstraw <agarwalp@tcd.ie>
2021-01-06 05:14:43 +0000

    firebase variables

--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,5 @@
+import firebase from 'firebase'
+
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
     apiKey: "AIzaSyAy1e9FCBs5yqquW_yp6qPiD9yK8jcO4HA",
@@ -8,3 +10,8 @@ const firebaseConfig = {
     appId: "1:74176226277:web:a847a7ec4451f51fc9450c",
     measurementId: "G-CES8W5J78K"
   };
+
+  const firebaseApp = firebase.initializeApp (firebaseConfig);
+  const db = firebaseApp.firestore();
+  const auth = firebase.auth()
+  const provider = new firebase.auth.GoogleAuthProvider();
