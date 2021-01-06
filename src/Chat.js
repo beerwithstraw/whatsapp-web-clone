0e954e0629766fdc28f18e44e718d4f36c6d395d
beerwithstraw <agarwalp@tcd.ie>
2021-01-06 04:10:10 +0000

    Chat Header design

--- src/Chat.js
+++ src/Chat.js
@@ -1,11 +1,40 @@
-import React from 'react'
+import { Avatar, IconButton } from '@material-ui/core'
+import { MoreVert, SearchOutlined } from '@material-ui/icons';
+import React, { useEffect, useState } from 'react'
 
 import './Chat.css'
 
 function Chat() {
+
+    const [seed, setSeed] = useState('');
+
+    useEffect(() => {
+        setSeed(Math.floor(Math.random() * 5000))
+    }, [])
+    
     return (
         <div className="chat">
-            
+            <div className="chat__header">
+                <Avatar src={`https://avatars.dicebear.com/api/human/${seed}.svg`}/>
+                <div className="chat__headerInfo">
+                    <h3>Group Name</h3>
+                    <p>Last Seen</p>
+                </div>
+                <div className="chat__headerRight">
+                    <IconButton>
+                        <SearchOutlined />
+                    </IconButton>
+                    <IconButton>
+                        <MoreVert />
+                    </IconButton>
+                </div>
+            </div>
+            <div className="chat__body">
+
+            </div>
+            <div className="chat__footer">
+
+            </div>
         </div>
     )
 }
