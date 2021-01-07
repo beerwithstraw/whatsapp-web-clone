445ee0fab1ac7cab4a24e975ad78fa9518067888
beerwithstraw <agarwalp@tcd.ie>
2021-01-07 02:10:55 +0000

    Sidebar Chat opening those particular chat rooms

--- src/Chat.js
+++ src/Chat.js
@@ -1,22 +1,31 @@
 import { Avatar, IconButton } from '@material-ui/core'
 import { AttachFile, InsertEmoticon, Mic, MoreVert, SearchOutlined } from '@material-ui/icons';
 import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom';
 
 import './Chat.css'
+import db from './firebase';
 
 function Chat() {
 
     const [seed, setSeed] = useState('');
     const [input, setInput] = useState('');
+    const { roomId } = useParams(); 
+    const [roomName, setRoomName] = useState('');
+
+    useEffect(() => {
+        if (roomId) {
+            db.collection("rooms").doc(roomId).onSnapshot((snapshot) => 
+                setRoomName(snapshot.data().name))
+        }
+    }, [roomId])
 
     useEffect(() => {
         setSeed(Math.floor(Math.random() * 5000))
-    }, [])
+    }, [roomId])
     
     const sendMessage = (e) => {
         e.preventDefault()
-        console.log(input)
-
         setInput("")
     }
     return (
@@ -24,7 +33,7 @@ function Chat() {
             <div className="chat__header">
                 <Avatar src={`https://avatars.dicebear.com/api/human/${seed}.svg`}/>
                 <div className="chat__headerInfo">
-                    <h3>Group Name</h3>
+                    <h3>{roomName}</h3>
                     <p>Last Seen</p>
                 </div>
                 <div className="chat__headerRight">
@@ -36,6 +45,7 @@ function Chat() {
                     </IconButton>
                 </div>
             </div>
+            
             <div className="chat__body">
                 <p className={`chat__message ${true && 'chat__receiver'}`}>
                     <span className="chat__name">Pulkit Agarwal</span>
