4928387abef950e22abed777e83552415bd0e1b4
beerwithstraw <agarwalp@tcd.ie>
2021-01-07 02:08:33 +0000

    Login Condition for User

--- src/App.js
+++ src/App.js
@@ -1,16 +1,38 @@
-import React from 'react'
-import Sidebar from './Sidebar';
+import React, { useState } from 'react'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
-import './App.css';
+import Sidebar from './Sidebar';
 import Chat from './Chat';
+import './App.css';
+import Login from './Login';
+
 
 function App() {
+
+  const [user, setUser] = useState(null)
+
   return (
     <div className="app">
-      <div class="app__body">
-        <Sidebar />  
-        <Chat />                 
+
+      {!user ? (
+        <Login />
+      ) : (
+        <div class="app__body"> 
+        <Router>
+          <Sidebar />
+          <Switch>
+            <Route path="/rooms/:roomId">
+              <Chat />
+            </Route>
+            <Route path="/">
+              {/* <Chat /> */}
+            </Route>
+          </Switch>
+        </Router>
+
       </div>
+
+      )}
     </div>
   );
 }
