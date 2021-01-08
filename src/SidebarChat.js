1736b8f8a0d86daa7e0ea82cff4a44b72a4ba0ed
beerwithstraw <agarwalp@tcd.ie>
2021-01-08 05:18:27 +0000

    Latest Message Update

--- src/SidebarChat.js
+++ src/SidebarChat.js
@@ -2,10 +2,22 @@ import { Avatar } from '@material-ui/core'
 import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom';
 import db from './firebase';
-
 import './SidebarChat.css'
+
 function SidebarChat({ addNewChat, id, name }) {
     const [seed, setSeed] = useState('');
+    const [messages, setMessages] = useState([]);
+    
+    const chatLogo = `https://avatars.dicebear.com/api/human/${seed}.svg`
+
+    useEffect(() => {
+        db.collection('rooms')
+            .doc(id).collection("messages")
+            .orderBy('timestamp', 'desc')
+            .onSnapshot(snapshot => (
+                setMessages(snapshot.docs.map(doc => doc.data()))
+            ))
+    }, [id])
 
     useEffect(() => {
         setSeed(Math.floor(Math.random() * 5000))
@@ -24,10 +36,11 @@ function SidebarChat({ addNewChat, id, name }) {
     return !addNewChat ? (
         <Link to={`/rooms/${id}`}>
             <div className="sidebarChat">
-                <Avatar src={`https://avatars.dicebear.com/api/human/${seed}.svg`}/>
+                 
+                <Avatar src={chatLogo}/>
                 <div className="sidebarChat__info">
                     <h2>{name}</h2>
-                    <p>Latest Message</p>
+                    <p>{messages[0]?.message}</p>
                 </div>
             </div>
         </Link>
@@ -36,6 +49,7 @@ function SidebarChat({ addNewChat, id, name }) {
             <h2>Add New Chat</h2>
         </div>
     )
+
 }
 
-export default SidebarChat
+export default SidebarChat;
